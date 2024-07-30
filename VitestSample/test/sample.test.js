f2cefe489735c6966022e4e4dcac59ef9bc25c2a
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2024-07-30 13:18:36 -0400

    adding vitest sample

---
+++ VitestSample/test/sample.test.js
@@ -0,0 +1,13 @@
+import { describe, it, expect, vi } from 'vitest'
+
+describe('Sabrina\'s Playlist Quality Tests', () => {
+    it('should be at least 8 hours long', () => {
+        expect(1 + 2).toBe(3);
+    });
+    it('should have at least 100 Brazil pop funk songs', () => {
+        expect(1 + 2).toBe(3);
+    });
+    it('should have at least 3 hours of Brazil rock n roll music', () => {
+        expect(1 + 2).toBe(3);
+    });
+});
